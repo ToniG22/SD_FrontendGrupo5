de9eb2402305ddac3aa492b5db8892a04e3770f2
ToniG22 <tonigarces19@gmail.com>
2023-11-02 12:41:58 +0000

    Updated form and table. ♻️

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Form = () => {
   const [inputs, setInputs] = useState({
     local: "",
-    date: "",
+    date: new Date(),
     time: "",
     participants: "",
   });
 
-  const handleChange = (event: { target: { name: any; value: any; }; }) => {
+  const handleChange = (event: { target: { name: any; value: any } }) => {
     const name = event.target.name;
     const value = event.target.value;
 
     setInputs((values) => ({ ...values, [name]: value }));
   };
 
-  const handleSubmit = (event: { preventDefault: () => void; }) => {
+  const handleDateChange = (date: Date | null, field: "date" | "time") => {
+    if (date) {
+      setInputs((values) => ({ ...values, [field]: date }));
+    }
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault();
     alert(JSON.stringify(inputs));
   };
@@ -33,19 +41,18 @@ const Form = () => {
       </label>
       <label>
         Date:
-        <input
-          type="date"
-          name="date"
-          value={inputs.date || ""}
-          onChange={handleChange}
+        <DatePicker
+          selected={inputs.date}
+          onChange={(date) => handleDateChange(date, "date")}
+          dateFormat="dd/MM/yyyy"
         />
       </label>
       <label>
         Time:
         <input
-          type="text"
+          type="time"
           name="time"
-          value={inputs.time || ""}
+          value={inputs.time}
           onChange={handleChange}
         />
       </label>
